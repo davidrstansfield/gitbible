5d6e70d6e6d9d313b746d921e9313af9b77eaf2b
David Stansfield <david@littlesystems.com.au>
2019-05-29 18:57:48 +1000

    Initial development

---
+++ index.php
@@ -0,0 +1,392 @@
+<?php
+/*
+  This file is part of gitBible.
+
+  Copyright 2019 David Stansfield (www.davidstansfield.id.au).
+
+  gitBible is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  gitBible is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with gitBible.  If not, see <https://www.gnu.org/licenses/>.
+*/
+?>
+<html>
+<head>
+<style>
+  body {
+    background-color: black;
+    color: silver;
+    font-family: georgia;
+  }
+  h1 {
+    color: gray;
+  }
+  .greek {
+    color: yellow;
+  }
+  .transliteration {
+    color: gray;
+  }
+  .translation {
+    color: gray;
+  }
+  .translation .text {
+    font-style: italic;
+  }
+  .source {
+    font-size:small;
+  }
+</style>
+</head>
+<body>
+<h1>gitBible</h1>
+<h2>Romans 1</h2>
+<h3>14</h3>
+<table>
+<tr class='greek'>
+  <td>&nbsp;</td>
+  <td>Ἕλλησίν</td>
+  <td>τε</td>
+  <td>καὶ</td>
+  <td>βαρβάροις</td>
+  <td>σοφοῖς</td>
+  <td>τε</td>
+  <td>καὶ</td>
+  <td>ἀνοήτοις</td>
+  <td>ὀφειλέτης</td>
+  <td>εἰμί</td>
+</tr>
+<tr class='transliteration'>
+  <td>&nbsp;</td>
+  <td>Hellēsin</td>
+  <td>te</td>
+  <td>kai</td>
+  <td>Barbarois</td>
+  <td>sophois</td>
+  <td>te</td>
+  <td>kai</td>
+  <td>anoētois</td>
+  <td>opheiletēs</td>
+  <td>eimi</td>
+</tr>
+<tr class='wordal'>
+  <td>&nbsp;</td>
+  <td>Greeks</td>
+  <td>both</td>
+  <td>and</td>
+  <td>Barbarians</td>
+  <td>wise</td>
+  <td>both</td>
+  <td>and</td>
+  <td>foolish</td>
+  <td>indebted</td>
+  <td>I am</td>
+</tr>
+<tr>
+  <td colspan='11'>&nbsp;</td>
+</tr>
+<tr class='translation'>
+  <td><span title='New Revised Standard Version'>NRSV</span></td>
+  <td class='text' colspan='10'>I am a debtor both to Greeks and to barbarians, both to the wise and to the foolish</td>
+</tr>
+<tr class='translation'>
+  <td><span title='The New American Bible, Revised Edition'>NABRE</span>&nbsp;</td>
+  <td class='text' colspan='10'>To Greeks and non-Greeks alike, to the wise and the ignorant, I am under obligation;</td>
+</tr>
+<tr class='translation'>
+  <td><span title='King James Version'>KJV</span></td>
+  <td class='text' colspan='10'>I am debtor both to the Greeks, and to the Barbarians; both to the wise, and to the unwise.</td>
+</tr>
+<tr class='translation'>
+  <td><span title='New International Version'>NIV</span></td>
+  <td class='text' colspan='10'>I am obligated both to Greeks and non-Greeks, both to the wise and the foolish.</td>
+</tr>
+</table>
+
+<h3>15</h3>
+<table>
+<tr class='greek'>
+  <td>&nbsp;</td>
+  <td>οὕτως</td>
+  <td>τὸ</td>
+  <td>κατ’</td>
+  <td>ἐμὲ</td>
+  <td>πρόθυμον</td>
+  <td>καὶ</td>
+  <td>ὑμῖν</td>
+  <td>τοῖς</td>
+  <td>ἐν</td>
+  <td>Ῥώμῃ</td>
+  <td>εὐαγγελίσασθαι</td>
+</tr>
+<tr class='transliteration'>
+  <td>&nbsp;</td>
+  <td>houtōs</td>
+  <td>to</td>
+  <td>kat’</td>
+  <td>eme</td>
+  <td>prothymon</td>
+  <td>kai</td>
+  <td>hymin</td>
+  <td>tois</td>
+  <td>en</td>
+  <td>Rhōmē</td>
+  <td>euangelisasthai</td>
+</tr>
+<tr class='wordal'>
+  <td>&nbsp;</td>
+  <td>in this manner</td>
+  <td>the</td>
+  <td>according to</td>
+  <td>me</td>
+  <td>eager</td>
+  <td>also</td>
+  <td>to you</td>
+  <td>the</td>
+  <td>in</td>
+  <td>Rome</td>
+  <td>to preach the gospel</td>
+</tr>
+<tr>
+  <td colspan='12'>&nbsp;</td>
+</tr>
+<tr class='translation'>
+  <td><span title='New Revised Standard Version'>NRSV</span></td>
+  <td class='text' colspan='11'>— hence my eagerness to proclaim the gospel to you also who are in Rome.</td>
+</tr>
+<tr class='translation'>
+  <td><span title='The New American Bible, Revised Edition'>NABRE</span>&nbsp;</td>
+  <td class='text' colspan='11'>that is why I am eager to preach the gospel also to you in Rome.</td>
+</tr>
+<tr class='translation'>
+  <td><span title='King James Version'>KJV</span></td>
+  <td class='text' colspan='11'>So, as much as in me is, I am ready to preach the gospel to you that are at Rome also.</td>
+</tr>
+<tr class='translation'>
+  <td><span title='New International Version'>NIV</span></td>
+  <td class='text' colspan='11'>That is why I am so eager to preach the gospel also to you who are in Rome.</td>
+</tr>
+</table>
+
+<h3>16</h3>
+<table>
+<tr class='greek'>
+  <td>&nbsp;</td>
+  <td>Οὐ</td>
+  <td>γὰρ</td>
+  <td>ἐπαισχύνομαι</td>
+  <td>τὸ</td>
+  <td>εὐαγγέλιον</td>
+  <td>τοῦ</td>
+  <td>χριστοῦ</td>
+  <td>δύναμις</td>
+  <td>γὰρ</td>
+  <td>θεοῦ</td>
+  <td>ἐστιν</td>
+  <td>εἰς</td>
+  <td>σωτηρίαν</td>
+  <td>παντὶ</td>
+  <td>τῷ</td>
+  <td>πιστεύοντι</td>
+  <td>Ἰουδαίῳ</td>
+  <td>τε</td>
+  <td>πρῶτον</td>
+  <td>καὶ</td>
+  <td>Ἕλληνι</td>
+</tr>
+<tr class='transliteration'>
+  <td>&nbsp;</td>
+  <td>Ou</td>
+  <td>gar</td>
+  <td>epaischynomai</td>
+  <td>to</td>
+  <td>euangelion</td>
+  <td>tou</td>
+  <td>Christou</td>
+  <td>dynamis</td>
+  <td>gar</td>
+  <td>Theou</td>
+  <td>estin</td>
+  <td>eis</td>
+  <td>sōtērian</td>
+  <td>panti</td>
+  <td>tō</td>
+  <td>pisteuonti</td>
+  <td>Ioudaiō</td>
+  <td>te</td>
+  <td>prōton</td>
+  <td>kai</td>
+  <td>Hellēni</td>
+</tr>
+<tr class='wordal'>
+  <td>&nbsp;</td>
+  <td>not</td>
+  <td>indeed</td>
+  <td>I am ashamed of</td>
+  <td>the</td>
+  <td>gospel</td>
+  <td>of the</td>
+  <td>Christ</td>
+  <td>power</td>
+  <td>indeed</td>
+  <td>of God</td>
+  <td>is</td>
+  <td>into</td>
+  <td>salvation</td>
+  <td>everyone</td>
+  <td>the</td>
+  <td>faithful</td>
+  <td>Jewish</td>
+  <td>both</td>
+  <td>first</td>
+  <td>and</td>
+  <td>Greek</td>
+</tr>
+<tr>
+  <td colspan='22'>&nbsp;</td>
+</tr>
+<tr class='translation'>
+  <td><span title='New Revised Standard Version'>NRSV</span></td>
+  <td class='text' colspan='21'>For I am not ashamed of the gospel; it is the power of God for salvation to everyone who has faith, to the Jew first and also to the Greek.</td>
+</tr>
+<tr class='translation'>
+  <td><span title='The New American Bible, Revised Edition'>NABRE</span>&nbsp;</td>
+  <td class='text' colspan='21'>For I am not ashamed of the gospel. It is the power of God for the salvation of everyone who believes: for Jew first, and then Greek.</td>
+</tr>
+<tr class='translation'>
+  <td><span title='King James Version'>KJV</span></td>
+  <td class='text' colspan='21'>For I am not ashamed of the gospel of Christ: for it is the power of God unto salvation to every one that believeth; to the Jew first, and also to the Greek.</td>
+</tr>
+<tr class='translation'>
+  <td><span title='New International Version'>NIV</span></td>
+  <td class='text' colspan='21'>For I am not ashamed of the gospel, because it is the power of God that brings salvation to everyone who believes: first to the Jew, then to the Gentile.</td>
+</tr>
+</table>
+
+<h3>17</h3>
+<table>
+<tr class='greek'>
+  <td>&nbsp;</td>
+  <td>Δικαιοσύνη</td>
+  <td>γὰρ</td>
+  <td>θεοῦ</td>
+  <td>ἐν</td>
+  <td>αὐτῷ</td>
+  <td>ἀποκαλύπτεται</td>
+  <td>ἐκ</td>
+  <td>πίστεως</td>
+  <td>εἰς</td>
+  <td>πίστιν</td>
+  <td>καθὼς</td>
+  <td>γέγραπται</td>
+  <td>Ὁ</td>
+  <td>δὲ</td>
+  <td>δίκαιος</td>
+  <td>ἐκ</td>
+  <td>πίστεως</td>
+  <td>ζήσεται</td>
+</tr>
+<tr class='transliteration'>
+  <td>&nbsp;</td>
+  <td>dikaiosynē</td>
+  <td>gar</td>
+  <td>Theou</td>
+  <td>en</td>
+  <td>autō</td>
+  <td>apokalyptetai</td>
+  <td>ek</td>
+  <td>pisteōs</td>
+  <td>eis</td>
+  <td>pistin</td>
+  <td>kathōs</td>
+  <td>gegraptai</td>
+  <td>Ho</td>
+  <td>de</td>
+  <td>dikaios</td>
+  <td>ek</td>
+  <td>pisteōs</td>
+  <td>zēsetai</td>
+</tr>
+<tr class='wordal'>
+  <td>&nbsp;</td>
+  <td>righteousness</td>
+  <td>indeed</td>
+  <td>of God</td>
+  <td>in</td>
+  <td>themselves</td>
+  <td>revealed</td>
+  <td>from</td>
+  <td>faithfulness</td>
+  <td>into</td>
+  <td>faith</td>
+  <td>as</td>
+  <td>it is written</td>
+  <td>the</td>
+  <td>and</td>
+  <td>righteous</td>
+  <td>from</td>
+  <td>faithfulness</td>
+  <td>will live</td>
+</tr>
+
+<tr>
+  <td colspan='18'>&nbsp;</td>
+</tr>
+<tr class='translation'>
+  <td><span title='New Revised Standard Version'>NRSV</span></td>
+  <td class='text' colspan='17'>For in it the righteousness of God is revealed through faith for faith; as it is written, ‘The one who is righteous will live by faith.’</td>
+</tr>
+<tr class='translation'>
+  <td><span title='The New American Bible, Revised Edition'>NABRE</span>&nbsp;</td>
+  <td class='text' colspan='17'>For in it is revealed the righteousness of God from faith to faith; as it is written, “The one who is righteous by faith will live.”</td>
+</tr>
+<tr class='translation'>
+  <td><span title='King James Version'>KJV</span></td>
+  <td class='text' colspan='17'>For therein is the righteousness of God revealed from faith to faith: as it is written, The just shall live by faith.</td>
+</tr>
+<tr class='translation'>
+  <td><span title='New International Version'>NIV</span></td>
+  <td class='text' colspan='17'>For in the gospel the righteousness of God is revealed — a righteousness that is by faith from first to last, just as it is written: “The righteous will live by faith.”</td>
+</tr>
+</table>
+
+<p>&nbsp;</p>
+
+<p><small>Copyright &copy; 2019 <a href="https://davidstansfield.id.au">David Stansfield</a>.<br>
+The <a href="https://github.com/davidrstansfield/gitbible">gitBible repository</a> is released under the <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU General Public License 3.0</a>.<br>
+<br>
+Sources are:<br>
+<table>
+<tr class='source'>
+  <td><span>Greek</span></td>
+  <td><a href='https://www.byzantinetext.com/'>ByzantineText.com</a>: <a href='https://www.byzantinetext.com/wp-content/uploads/2017/08/RP2016-Readers-Edition.pdf'>The Greek New Testament For Beginning Readers, Byzantine Textform</a> (PDF)</td>
+</tr>
+<tr class='source'>
+  <td><span title='New Revised Standard Version'>NRSV&nbsp;*</span></td>
+  <td><a href='https://bible.oremus.org/'>oremus Bible Browser</a></td>
+</tr>
+<tr class='source'>
+  <td><span title='The New American Bible, Revised Edition'>NABRE&nbsp;*</span>&nbsp;</td>
+  <td><a href='http://usccb.org'>United States Catholic Conference of Bishops</a>: <a href='http://usccb.org/bible/'>Bible</a></td>
+</tr>
+<tr class='source'>
+  <td><span title='King James Version'>KJV</span></td>
+  <td><a href='https://www.bible.com/bible'>Bible.com</a></td>
+</tr>
+<tr class='source'>
+  <td><span title='New International Version'>NIV&nbsp;*</span></td>
+  <td><a href='https://www.biblegateway.com/passage/?version=KJV'>BibleGateway.com</a></td>
+</tr>
+</table>
+* Copyright to be confirmed.
+</small></p>
+</body>
+</html>
